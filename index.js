d4e2fdebf260d8198d2c68ba0b863713593c282b
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-05-07 13:49:29 -0400

    Completed employee inputs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
 const inquire = require("inquirer");
+const Intern = require("./lib/Intern");
 
 
 const teamArray = [];
@@ -11,7 +13,7 @@ const teamArray = [];
 
 function initApp() {
     
-    function buildManager() {
+    function addManager() {
 
         // prompted to enter the team manager’s name, employee ID, email address, and office number
         
@@ -31,7 +33,7 @@ function initApp() {
             {
                 type: "input",
                 name: "empId",
-                message: "Provide Team Manager's employee ID number.",
+                message: "Provide Team Manager's 5 digit employee ID number.",
                 validate: answer => {
                     const valid = answer.match( /^[1-9]{1,5}$/ );
                     if (valid) {
@@ -49,19 +51,19 @@ function initApp() {
                     if (valid) {
                         return true;
                     }
-                    return "Please provide valid emplyoee email (must in @guycorp.com)";
+                    return "Please provide valid emplyoee email (must end in @guycorp.com).";
                 }
             },
             {
                 type: "input",
                 name: "officeNumber",
-                message: "What is the Team Manager's office number?",
+                message: "What is the Team Manager's 10 digit office number?",
                 validate: answer => {
                     const valid = answer.match(/^[0-9]{10,10}$/ );
                     if (valid) {
                         return true;
                     }
-                    return "Please provide valid emplyoee email (must in @guycorp.com)";
+                    return "Please provide valid office number.";
                 }
             }
             
@@ -69,10 +71,155 @@ function initApp() {
             const manager = new Manager(answers.name, answers.empId, answers.email, answers.officeNumber);
             teamArray.push(manager);
             console.log(manager);
-          });   
+            buildTeam();
+          });  
+        };
+        
+
+    function buildTeam() {
+        inquire.prompt([
+            
+            {
+                type: "list",
+                name: "empChoice",
+                message: "What team memeber are you adding to the team?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "No new members to add."
+                ]
+            }
+        ]).then(choice => {
+            switch(choice.empChoice) {
+                case "Engineer": addEngineer();
+                break;
+                case "Intern": addIntern();
+                break;
+                //default: saveTeam();
+
+            }
+        });
     };
+
+    function addEngineer() {
+
+        inquire.prompt([
+            
+            {
+                type: "input",
+                name: "name",
+                message: "Provide Engineer's name.",
+                validate: answer => {
+                    if (answer !== ""){
+                        return true;
+                    }
+                    return "Please provide Engineer's name.";
+                }
+            },
+            {
+                type: "input",
+                name: "empId",
+                message: "Provide Engineer's 5 digit employee ID number.",
+                validate: answer => {
+                    const valid = answer.match( /^[1-9]{1,5}$/ );
+                    if (valid) {
+                        return true;
+                    }
+                    return "Please provide valid emplyoee number.";
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the Engineer's email?",
+                validate: answer => {
+                    const valid = answer.match( /@guycorp.com$/ );
+                    if (valid) {
+                        return true;
+                    }
+                    return "Please provide valid emplyoee email (must end in @guycorp.com)";
+                }
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "Enter Engineer's GitHub user name.",
+                validate: answer => {
+                    if (answer !== ""){
+                        return true;
+                    }
+                    return "Please provide Engineer's GitHub username.";
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.name, answers.empId, answers.email, answers.gitHub);
+            teamArray.push(engineer);
+            console.log(engineer);
+            buildTeam();
+          });
+
+    };
+
+    function addIntern() {
+
+        inquire.prompt([
+            
+            {
+                type: "input",
+                name: "name",
+                message: "Provide Intern's name.",
+                validate: answer => {
+                    if (answer !== ""){
+                        return true;
+                    }
+                    return "Please provide Intern's name.";
+                }
+            },
+            {
+                type: "input",
+                name: "empId",
+                message: "Provide Intern's 5 digit employee ID number.",
+                validate: answer => {
+                    const valid = answer.match( /^[1-9]{1,5}$/ );
+                    if (valid) {
+                        return true;
+                    }
+                    return "Please provide valid emplyoee number.";
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the Intern's email?",
+                validate: answer => {
+                    const valid = answer.match( /@guycorp.com$/ );
+                    if (valid) {
+                        return true;
+                    }
+                    return "Please provide valid emplyoee email (must end in @guycorp.com)";
+                }
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Enter Intern's school.",
+                validate: answer => {
+                    if (answer !== ""){
+                        return true;
+                    }
+                    return "Please provide Intern's school.";
+                }
+            }
+        ]).then(answers => {
+            const intern = new Intern(answers.name, answers.empId, answers.email, answers.school);
+            teamArray.push(intern);
+            console.log(intern);
+            buildTeam();
+          });
+
+    }
    
-    buildManager();
+    addManager();
     
 }
 
